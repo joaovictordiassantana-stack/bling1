cb9af2ef64735891358a11a8838445ef398a259b
João Victor <joaovictordiassantana-stack@users.noreply.github.com>
2025-11-24 13:49:56 -0300

    Finaliza start.sh com Waitress e atualiza requirements.txt

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Define a porta de bind usando a variável de ambiente PORT
 # O Waitress usará esta variável
-WAITRESS_CMD="waitress-serve --listen=0.0.0.0:$PORT bling:app"
+WAITRESS_CMD="waitress-serve --listen=0.0.0.0:$PORT --call bling:create_app"
 
 echo "Iniciando com o comando: $WAITRESS_CMD"
 
